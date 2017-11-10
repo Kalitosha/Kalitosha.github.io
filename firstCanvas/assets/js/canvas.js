320834d30032217dc715a307bebc793b24945553
Kalitosha <burtseva_maria@rambler.ru>
2017-11-11 01:39:35 +0300

    123456789

--- firstCanvas/assets/js/canvas.js
+++ firstCanvas/assets/js/canvas.js
@@ -1,12 +1,12 @@
 ﻿var canvas = document.getElementById('myCanvas'); // подключаем canvas
 var ctx = canvas.getContext('2d'); // задаем контекст
 
-document.body.addEventListener('touchmove', function(event) {
-    event.preventDefault();
-}, false); // Предотвращение скролла
-
 window.addEventListener('resize', resizeCanvas, false);
 // canvas.addEventListener("touchmove", touchMove, false);
+
+document.body.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+}, false); // Предотвращение скролла
 
 // var Width, Height;
 
@@ -15,6 +15,9 @@ Height = document.documentElement.clientHeight;
 
 
 var gall = 0;
+var imgBall = new Image();  // Создание нового объекта изображения
+var imgPlatform = new Image();
+
 window.onload = function () {
 
     canvas.width = window.innerWidth;
@@ -23,8 +26,13 @@ window.onload = function () {
     canvas.width = document.documentElement.clientWidth;
     canvas.height = document.documentElement.clientHeight;
 
-    // Width = document.documentElement.clientWidth;
-    // Height = document.documentElement.clientHeight;
+
+    var fonMus = document.getElementById('fonMusic'); // фоновая музыка
+    //fonMus.play();
+    fonMus.volume = 0.1;
+
+    imgBall.src ='assets\\images\\ball.png';  // Путь к изображению которое необходимо нанести на холст
+    imgPlatform.src = 'assets\\images\\pl.png';
 
     init();
 }
@@ -34,18 +42,29 @@ var mouseMove = function(e) {
     platform2.y = e.clientY - platform2.h/2;
 }
 
-var touchMove = function (e) {
+// var touchMove = function (e) {
+document.addEventListener('touchmove', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        /* Здесь ваш код обработки события*/
+    if (e.targetTouches.length == 1) {
+        var touch = e.targetTouches[0];
 
+        platform1.y = touch.pageY - platform1.h / 2;
+        platform2.y = touch.pageY - platform2.h / 2;
+    }
+    }, false);
     // if (event.targetTouches.length == 1) {
     //     var touch = event.targetTouches[0];
     //     // Place element where the finger is
     //     obj.style.left = touch.pageX + 'px';
     //     obj.style.top = touch.pageY + 'px';
     // }
-
-    platform1.y = e.touchAction.pageY - platform1.h/2;
-    platform2.y = e.touchAction.pageY - platform2.h/2;
-}
+//     if (event.targetTouches.length == 1) {
+//         platform1.y = e.touchAction.pageY - platform1.h / 2;
+//         platform2.y = e.touchAction.pageY - platform2.h / 2;
+//     }
+// }
 
 var ball = {
     color : 'black',
@@ -55,7 +74,8 @@ var ball = {
     x : 0,
     y : 0,
     drawBall : function () {
-    }
+    },
+    drawSprite : function () {}
 }
 
 function Rect(color, x, y) {
@@ -63,25 +83,29 @@ function Rect(color, x, y) {
     this.x = x; // координата х
     this.y = y; // координата у
     this.h = Height / 6; // высота
-    this.w = Height / 6 / 15; // ширина
+    this.w = Height / 6 / 10; // ширина
     this.score = 0;
     this.drawRect = function () {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.w, this.h);
-    }
+    };
+    this.drawSprite = function (){
+        ctx.drawImage(imgPlatform, this.x, this.y, this.w, this.h);
+    };
 }
 
 
 function init() {
 
     platform1 = new Rect('rgb(255, 100, 0)', 1, Height/2 - Height/6/2);
-    platform2 = new Rect('rgb(255, 100, 0)', Width - Height/6/15 - 1, Height/2 - Height/6/2);
+    platform2 = new Rect('rgb(255, 100, 0)', Width - Height/6/10 - 1, Height/2 - Height/6/2);
+
 
     ball = {
         color : 'RGB(255, 180, 0)',
-        r :  Height/60,
-        vx : 3,
-        vy : -3,
+        r :  Height/30,
+        vx : -6,
+        vy : -6,
         x : Width/2,
         y : Height/2,
         drawBall : function () {
@@ -90,8 +114,12 @@ function init() {
             ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, true);
             ctx.fill();
             ctx.closePath();
+         },
+
+        drawSprite : function () {
+                ctx.drawImage(imgBall, this.x, this.y, this.r, this.r);
         }
-    }
+    };
     //ball = new Ball('RGB(250, 220, 100)', Width/2, Height/2, Height/40);
 
     draw();
@@ -106,22 +134,22 @@ function resizeCanvas() {
         /*************************************************************************************/
         //platform1.x = platform1.x * document.documentElement.clientWidth/Width;
         platform1.y = platform1.y * document.documentElement.clientHeight/Height;
-        platform1.w = document.documentElement.clientHeight / 6 / 15;
+        platform1.w = document.documentElement.clientHeight / 6 / 10;
         platform1.h = document.documentElement.clientHeight / 6;
 
-        platform2.x = document.documentElement.clientWidth - document.documentElement.clientHeight / 6 / 15 - 1;
+        platform2.x = document.documentElement.clientWidth - document.documentElement.clientHeight / 6 / 10 - 1;
         platform2.y = platform2.y * document.documentElement.clientHeight/Height;
-        platform2.w = document.documentElement.clientHeight / 6 / 15;
+        platform2.w = document.documentElement.clientHeight / 6 / 10;
         platform2.h = document.documentElement.clientHeight / 6;
 
-        ball.r = document.documentElement.clientHeight/60;
+        ball.r = document.documentElement.clientHeight/30;
         if (gall == 1){
 
             ball.x = platform2.x - ball.r;
         }
         else if (gall == 2){
 
-            ball.x = platform1.x + platform1.w + ball.r;
+            ball.x = platform1.x + platform1.w; // + ball.r;
         }
         else{
             ball.x = ball.x * document.documentElement.clientWidth/Width;
@@ -135,6 +163,7 @@ function resizeCanvas() {
 
 function draw() {
     ctx.clearRect(0, 0, Width, Height); // стираем
+
 
     ctx.strokeStyle = 'rgb(67, 100, 100)';
     ctx.beginPath();
@@ -150,11 +179,6 @@ function draw() {
     // ctx.fillText(Height, 20, 20);
     // ctx.fillText(Width, 70, 20);
 
-    // ctx.fillStyle = 'rgb(80, 110, 110)';
-    // ctx.font = "15px Comic Sans MS";
-    // ctx.fillText(ball.vx, 170, 20);
-    // ctx.fillText(ball.vy, 170, 40);
-
     //счет
     ctx.fillStyle = 'rgb(80, 110, 110)';
     ctx.font = "100px Comic Sans MS";
@@ -165,16 +189,19 @@ function draw() {
     ctx.fillText(platform2.score, Width/2 + 10, Height/2 + 34);
 
 
-    ball.drawBall();
-    platform1.drawRect();
-    platform2.drawRect();
+    ball.drawSprite();
+    // ball.drawBall();
+    platform1.drawSprite();
+    platform2.drawSprite();
+    // platform1.drawRect();
+    // platform2.drawRect();
 
     collision();
 
     canvas.onmousemove = mouseMove;
     canvas.onmousedown = onMouseDown;
-    canvas.touchmove = touchMove; // TODO проверить
-    canvas.touchend = onMouseDown; // убрали палец
+    // canvas.touchmove = touchMove; // TODO проверить
+    canvas.touchstart = onMouseDown; // Tap (Косание)
 
     window.requestAnimationFrame(draw);
 }
@@ -190,17 +217,17 @@ function draw() {
 
 function collision() {
     if (gall == 0) {
-        if ((ball.y - ball.r < 0) || (ball.y + ball.r > Height)) { // стена: верх, низ
+        if ((ball.y < 0) || (ball.y + ball.r > Height)) { // стена: верх, низ
             ball.vy = -ball.vy;
         }
 
-        else if (ball.x - ball.r < platform1.x + platform1.w) { // столкновение с ракеткой №1
-            if ((ball.y + ball.r > platform1.y) && (ball.y - ball.r < platform1.y + platform1.h )) // на промежутке
+        else if (ball.x < platform1.x + platform1.w) { // столкновение с ракеткой №1
+            if ((ball.y + ball.r > platform1.y) && (ball.y < platform1.y + platform1.h )) // на промежутке
                 ball.vx = -ball.vx;
             else { // стена: лево // вне промежутка нахождения ракетки
                 platform2.score++;
                 ball.x = platform2.x - ball.r;
-                ball.y = platform1.y + platform1.h / 2;
+                ball.y = platform2.y + platform2.h / 2 - ball.r/2;
                 gall = 1;
                 return;//ГОЛЛ!
             }
@@ -212,8 +239,8 @@ function collision() {
             }
             else {  // стена: право
                 platform1.score++;
-                ball.x = platform1.x + platform1.w + ball.r;
-                ball.y = platform2.y + platform2.h / 2;
+                ball.x = platform1.x + platform1.w; // + ball.r;
+                ball.y = platform2.y + platform2.h / 2 - ball.r/2;
                 gall = 2;
                 return;//ГОЛЛ!
             }
@@ -224,15 +251,16 @@ function collision() {
     }
     else
         // if (gall == 1){
-            ball.y = platform2.y + platform2.h / 2; // одно и тоже
+            ball.y = platform2.y + platform2.h / 2 - ball.r/2; // одно и тоже
     // }
     // else
     //     if (gall == 2){
-    //         ball.y = platform1.y + platform1.h / 2; // одно и тоже
+    //         ball.y = platform1.y + platform1.h / 2 - ball.r/2; // одно и тоже
     // }
 }
 
 function onMouseDown(e) {
+
     if (gall != 0) {
         ball.vx = -ball.vx;
         ball.x += ball.vx;
