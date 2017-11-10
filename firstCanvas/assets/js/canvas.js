65dbd1b399ccf6925fa28c8802b8d2cfa1e586bc
Kalitosha <burtseva_maria@rambler.ru>
2017-11-10 17:16:05 +0300

    1

--- firstCanvas/assets/js/canvas.js
+++ firstCanvas/assets/js/canvas.js
@@ -144,8 +144,7 @@ function draw() {
     // ctx.fillText(ball.vy, 170, 40);
 
     //счет
-    // ctx.fillStyle = 'rgb(80, 110, 110)';
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = 'rgb(80, 110, 110)';
     ctx.font = "100px Comic Sans MS";
     ctx.fillText(":", Width/2 - 14, Height/2 + 31);
     ctx.textAlign = "right";
